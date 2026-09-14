48c9a72caaebb4572ffd5bbe169ce8fe71cb78aa
Kan-O435 <ky.20626314@gmail.com>
2026-09-14 18:25:22 +0900

    add temple body and link glb model assets

---
+++ internal/assets/assets.go
@@ -0,0 +1,12 @@
+// Package assets は、3Dモデル等のバイナリアセットをgo:embedでコンパイル時に
+// バイナリへ埋め込む。ブラウザ側で別途fetch()する手間を省くための簡易的な方法で、
+// アセット数が増えてきたらHTTP fetch方式への切り替えを検討する。
+package assets
+
+import _ "embed"
+
+//go:embed models/temple-body.glb
+var TempleBody []byte
+
+//go:embed models/link-knight.glb
+var LinkKnight []byte
